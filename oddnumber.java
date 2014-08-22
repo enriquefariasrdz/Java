e20c08390f21c50fae59a16a07ce648487e3832d
enriquefariasrdz <enriquefariasrdz@gmail.com>
2014-08-22 14:58:23 -0500

    Create oddnumber.java

---
+++ oddnumber.java
@@ -0,0 +1,25 @@
+/**
+ * Created by enriquef on 8/20/14.
+ */
+public class Jo {
+
+
+    public static void main (String args[]){
+
+        for (int i= 3; i < 20 ; i= i+3){
+
+            if(i%2 ==0){
+
+                System.out.println("This is an odd number "+ i);
+
+            }else{
+
+                System.out.println(i);
+
+            }
+
+        }
+
+    }
+
+}
